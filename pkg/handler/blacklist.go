eb6a89979db1b0abaa46cd1f8a43aee3728ade35
Mark Raiter <raitermrk@gmail.com>
2023-05-24 23:43:28 +0300

    ...

---
+++ pkg/handler/blacklist.go
@@ -0,0 +1,19 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func (h *Handler) addToBlacklist(c echo.Context) error {
+	return nil
+}
+
+func (h *Handler) getAllBlocked(c echo.Context) error {
+	return nil
+}
+
+func (h *Handler) getBlocked(c echo.Context) error {
+	return nil
+}
+
+func (h *Handler) deleteFromBlacklist(c echo.Context) error {
+	return nil
+}
---
+++ pkg/handler/friendlist.go
@@ -0,0 +1,19 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func (h *Handler) addToFriends(c echo.Context) error {
+	return nil
+}
+
+func (h *Handler) getAllFriends(c echo.Context) error {
+	return nil
+}
+
+func (h *Handler) getFriend(c echo.Context) error {
+	return nil
+}
+
+func (h *Handler) deleteFriend(c echo.Context) error {
+	return nil
+}
