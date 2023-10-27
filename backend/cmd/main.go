925f7be811d043ac925ee5a0e1328469db2eb438
Mark Raiter <raitermrk@gmail.com>
2023-10-28 00:29:31 +0300

    some db initialization with docker

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"log"
+
+	"github.com/markraiter/chat/internal/storage/postgres"
+)
+
+func main() {
+	_, err := postgres.NewDB()
+	if err != nil {
+		log.Fatalf("could not initialize database connection: %s\n", err.Error())
+	}
+}
